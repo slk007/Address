404740d2be39610e9e3ead8e215b17bff29f5813
slk007 <shubham.soni.lko@gmail.com>
2020-04-30 19:58:01 +0530

    created address model

--- Address/settings.py
+++ Address/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = 'Address.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
+
+DATABASES['default'] = dj_database_url.config(default='postgres://yticuaxqmnuxre:f87a98fba755b74880fb51c4921115508f4f1cc8db368cb17eaf1a86f6eb11c8@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d2f1dp649nqhln')
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
--- addressbook/models.py
+++ addressbook/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=30)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=15)
+    state = models.CharField(max_length=15)
+    zipcode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.name
+    
