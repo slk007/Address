8a245c734f99828f065fbc0cf2e1b252414d8d29
slk007 <shubham.soni.lko@gmail.com>
2020-04-30 20:43:36 +0530

    done

--- addressbook/views.py
+++ addressbook/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Address
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    all_addresses = Address.objects.all
+    return render(request, 'home.html', {"all_addresses": all_addresses})
 
 def add_address(request):
     return render(request, 'add_address.html', {})
